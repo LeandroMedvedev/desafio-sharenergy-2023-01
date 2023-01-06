200d3ea469bf96079ec3f6941cdefcfd03822784
Leandro Medvedev <leandromedvedev@hotmail.com>
2023-01-06 03:11:47 -0300

    chore: initial folder structure

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import mongoose from 'mongoose';
+
+import app from './app';
+
+const db = process.env.DB;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+mongoose.set('strictQuery', true);
+mongoose
+  .connect(
+    `mongodb+srv://${dbUser}:${dbPassword}@cluster0.qrv9jws.mongodb.net/${db}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log('Connected database!');
+    const PORT = process.env.PORT || 3333;
+
+    app.listen(PORT, () =>
+      console.log(`App running!\nhttp://localhost:${PORT}`)
+    );
+  })
+  .catch((error) => console.log(error));
