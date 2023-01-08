b88eb23933c1c54335f1792c6eed155c1c38405a
Leandro Medvedev <leandromedvedev@hotmail.com>
2023-01-08 01:10:21 -0300

    feat: get user by username route concluded

--- src/controllers/listUsers.controller.ts
+++ src/controllers/listUsers.controller.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from 'express';
+import { IUser } from '../interfaces';
 
 import { listUsersService } from '../services';
 
-export const listUsersController = async (_: Request, response: Response) => {
-  const { status, message } = await listUsersService();
+export const listUsersController = async (
+  request: Request,
+  response: Response
+) => {
+  const { status, message } = await listUsersService(
+    request.query as Pick<IUser, 'username'>
+  );
 
   return response.status(status).json(message);
 };
--- src/services/listUsers.service.ts
+++ src/services/listUsers.service.ts
@@ -2,7 +2,19 @@ import { IUser } from '../interfaces';
 import { User } from '../models';
 import { serializedUserSchema } from '../schemas';
 
-export const listUsersService = async () => {
+export const listUsersService = async ({
+  username,
+}: Pick<IUser, 'username'>) => {
+  // if username query param IS passed
+  if (username) {
+    const user = await User.findOne({ username: username });
+    const serialized = await serializedUserSchema.validate(user, {
+      stripUnknown: true,
+    });
+    return { status: 200, message: serialized };
+  }
+
+  // if username query param IS NOT passed
   const users = await User.find();
 
   const serialized: Array<Partial<IUser>> = await Promise.all(
